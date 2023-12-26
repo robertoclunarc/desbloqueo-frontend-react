2e092a1b3478a08656cac34ff33aa9603b00cccc
robertoclunarc <robertoclunarg@gmail.com>
2023-12-26 13:44:54 -0400

    create orden

--- src/pages/resumenPago/resumenPago.jsx
+++ src/pages/resumenPago/resumenPago.jsx
@@ -1,39 +1,48 @@
-import {
-  Container,
-  Typography,
-} from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import React from 'react';
+import { useParams } from 'react-router';
 import ResumenPagoForm from './resumenPagoForm';
+import Navbar from '../../components/navbar/Navbar';
 
 function ResumenPago() {
+  const { status } = useParams();
+  // eslint-disable-next-line no-console
+  console.log(status);
   return (
-    <Container sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      gap: '10px',
-      margin: '.5em',
-    }}
-    >
-      <Typography
-        variant="h4"
-        sx={{
-          color: 'white',
-        }}
+    <>
+      <Navbar />
+      <Container sx={{
+        padding: '5px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '10px',
+        paddingTop: '50px',
+        marginTop: '6vh',
+        paddingBottom: '6vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#0F5986',
+      }}
       >
-        Resumen
-      </Typography>
-      <ResumenPagoForm />
-      <Typography
-        variant="h6"
-        sx={{
-          color: 'white',
-        }}
-      >
-        djfsdjkfsbfsdbfjshfjksdfjkbsdkfjbhsdjkfsdkbsdkjfsdjkffsdhf.
-      </Typography>
-    </Container>
+        <Typography
+          variant="h4"
+          sx={{
+            color: 'white',
+          }}
+        >
+          {status}
+        </Typography>
+        <ResumenPagoForm />
+        <Typography
+          variant="h6"
+          sx={{
+            color: 'white',
+          }}
+        >
+          Pronto estara recibiendo respuesta al correo que suministro. Gracias!
+        </Typography>
+      </Container>
+    </>
   );
 }
 
--- src/pages/resumenPago/resumenPagoForm.jsx
+++ src/pages/resumenPago/resumenPagoForm.jsx
@@ -1,22 +1,62 @@
+/* eslint-disable no-console */
 /* eslint-disable react/forbid-prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Typography } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router';
-import { setOpcionesStore } from '../../store/slices/opciones.slice';
+import { setOpcionesStore, setOpcionesGlobal } from '../../store/slices/opciones.slice';
+import postCreateOrdenDrSim from '../../api/drsimcreateordenes';
+import putDynamobdOrden from '../../api/putDynamodbOrden';
 
-function ResumenPagoForm() {
+async function createOrden(idTerminal, idOperador, imei, idService) {
+  let servicios = [];
+  console.log(`term:${idTerminal}, oper:${idOperador}, imei:${imei}, serv:${idService}`);
+  await postCreateOrdenDrSim(idTerminal, idOperador, imei, idService)
+    .then((respuesta) => {
+      servicios = respuesta;
+      console.log(respuesta);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return servicios;
+}
+
+async function ResumenPagoForm() {
   const { status } = useParams();
-  // eslint-disable-next-line no-console
-  console.log(status);
   const datosResumen = localStorage.getItem('datosResumen');
   const dispatch = useDispatch();
+  const [msnSolicitud, setMsnSolicitud] = useState('');
+  const [loading, setLoading] = useState(false);
   dispatch(setOpcionesStore(datosResumen));
   const opcionesString = useSelector((state) => state.opciones);
   const opciones = JSON.parse(opcionesString);
-  // eslint-disable-next-line no-console
-  console.log(opciones);
-  localStorage.removeItem('datosResumen');
+  if (status === 'success') {
+    const price = opciones[5]?.price;
+    const idTerminal = opciones[3].idReg;
+    const idOperador = opciones[1].idReg;
+    const { imei } = opciones[10] !== undefined ? opciones[10] : '';
+    const { email } = opciones[11] !== undefined ? opciones[11] : '';
+    const idService = opciones[4].idReg;
+    const ticket = await createOrden(idTerminal, idOperador, imei, idService);
+    if (ticket?.res?.id_ticket) {
+      const timestamp = Date.now();
+      const fecha = new Date(timestamp);
+      const hoy = fecha.toISOString();
+      putDynamobdOrden(timestamp, `${ticket?.res.id_ticket}`, hoy, email, `${imei}`, idService, `${price}`, 'PENDIENTE');
+      setMsnSolicitud(ticket);
+      dispatch(setOpcionesGlobal({ id: '12', id_ticket: `${ticket?.res.id_ticket}` }));
+    } else {
+      setMsnSolicitud('Solicitud: NO Procesada!');
+      if (ticket?.res?.id_ticket === undefined) {
+        setLoading(true);
+      }
+    }
+    console.log(msnSolicitud);
+    console.log(loading);
+    console.log(ticket);
+  }
+
   return (
     <Box
       sx={{
@@ -86,13 +126,23 @@ function ResumenPagoForm() {
 
       </Typography>
       <Typography
-        sx={{ color: 'black', fontSize: '20px' }}
+        sx={{ color: 'black' }}
       >
         Monto Pagado:
         <span>  </span>
         <span style={{ fontWeight: 'bold' }}>
           $
           {opciones[5]?.price}
+        </span>
+      </Typography>
+      <Typography
+        sx={{ color: 'black' }}
+      >
+        Nro. Ticket:
+        <span>  </span>
+        <span style={{ fontWeight: 'bold' }}>
+          $
+          {msnSolicitud?.status}
         </span>
       </Typography>
     </Box>
