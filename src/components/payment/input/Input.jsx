03bad24b6bc73e721c71bd90c8aac4eab707f3b4
James <jamesdev4you@gmail.com>
2024-02-02 12:13:50 -0500

    validation fix. branch done

--- src/components/payment/input/Input.jsx
+++ src/components/payment/input/Input.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable react/prop-types */
-import {
-  Box, TextField, Typography,
-} from '@mui/material';
+import { Box, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setOpcionesGlobal } from '../../../store/slices/opciones.slice';
@@ -24,8 +22,9 @@ function isValidIMEI(n) {
 
   let sum = 0;
   // eslint-disable-next-line no-plusplus
-  for (let i = len; i >= 1; i--) { // Corrected this line
-    let d = (n % 10);
+  for (let i = len; i >= 1; i--) {
+    // Corrected this line
+    let d = n % 10;
 
     if (i % 2 === 0) d *= 2;
 
@@ -34,7 +33,7 @@ function isValidIMEI(n) {
     n = parseInt(n / 10, 10);
   }
 
-  return (sum % 10 === 0);
+  return sum % 10 === 0;
 }
 function Input() {
   const [valueOptions, setValueOptions] = useState('');
@@ -47,30 +46,47 @@ function Input() {
   const handleChangeEmail = (event) => {
     const inputValue = event.target.value;
     const emailPattern = /^\S+@\S+\.\S+$/;
+    dispatch(setOpcionesGlobal({ id: '6', email: inputValue, emailValid: false }));
     if (emailPattern.test(inputValue)) {
       setValueOptionsEmail(inputValue);
-      dispatch(setOpcionesGlobal({ id: '6', email: inputValue }));
-      setMsgCorreo('');
+      dispatch(
+        setOpcionesGlobal({ id: '6', email: inputValue, emailValid: true }),
+      );
+      setMsgCorreo('Gracias!');
     } else {
       setMsgCorreo('Por favor ingresa un Correo electronico valido');
+      dispatch(
+        setOpcionesGlobal({ id: '6', email: inputValue, emailValid: false }),
+      );
     }
   };
 
   const handleChangeImei = (event) => {
     const inputValue = event.target.value;
     setValueOptions(inputValue);
+    dispatch(setOpcionesGlobal({ id: '5', imei: inputValue, imeiValid: false }));
     if (isValidIMEI(inputValue)) {
-      dispatch(setOpcionesGlobal({ id: '5', imei: inputValue }));
-      setMsgImei('');
+      dispatch(
+        setOpcionesGlobal({ id: '5', imei: inputValue, imeiValid: true }),
+      );
+      setMsgImei('Gracias!');
     } else {
       setMsgImei('Por favor ingrese un imei valido');
+      dispatch(
+        setOpcionesGlobal({ id: '5', imei: inputValue, imeiValid: false }),
+      );
     }
   };
 
   return (
-    <Box sx={{
-      display: 'flex', width: { xs: '90%', sm: '60%' }, flexDirection: 'column', padding: '20px', borderRadius: '15px',
-    }}
+    <Box
+      sx={{
+        display: 'flex',
+        width: { xs: '90%', sm: '60%' },
+        flexDirection: 'column',
+        padding: '20px',
+        borderRadius: '15px',
+      }}
     >
       <Typography sx={{ color: 'white', fontSize: '20px', textAlign: 'left' }}>
         {' '}
@@ -79,19 +95,43 @@ function Input() {
         <span style={{ color: 'red' }}>*</span>
         {' '}
       </Typography>
-      <Typography sx={{ color: '#C62907', fontSize: '15px' }}>{msgImei}</Typography>
-      <TextField sx={{ backgroundColor: 'white' }} color="secondary" id="imei" label="IMEI" variant="filled" onChange={handleChangeImei} defaultValue={opciones[10]?.imei || valueOptions} />
-      <Typography sx={{
-        color: 'white', fontSize: '20px', paddingTop: '15px', textAlign: 'left',
-      }}
+      <Typography sx={{ color: '#C62907', fontSize: '15px' }}>
+        {msgImei}
+      </Typography>
+      <TextField
+        sx={{ backgroundColor: 'white' }}
+        color="secondary"
+        id="imei"
+        label="IMEI"
+        variant="filled"
+        onChange={handleChangeImei}
+        defaultValue={opciones[10]?.imei || valueOptions}
+      />
+      <Typography
+        sx={{
+          color: 'white',
+          fontSize: '20px',
+          paddingTop: '15px',
+          textAlign: 'left',
+        }}
       >
         {' '}
         Ingresa tu Correo electronico
         <span style={{ color: 'red' }}>*</span>
         {' '}
       </Typography>
-      <Typography sx={{ color: '#C62907', fontSize: '15px' }}>{msgCorreo}</Typography>
-      <TextField sx={{ backgroundColor: 'white' }} color="secondary" id="email" label="Correo electronico" variant="filled" onChange={handleChangeEmail} defaultValue={opciones[11]?.email || valueOptionsEmail} />
+      <Typography sx={{ color: '#C62907', fontSize: '15px' }}>
+        {msgCorreo}
+      </Typography>
+      <TextField
+        sx={{ backgroundColor: 'white' }}
+        color="secondary"
+        id="email"
+        label="Correo electronico"
+        variant="filled"
+        onChange={handleChangeEmail}
+        defaultValue={opciones[11]?.email || valueOptionsEmail}
+      />
     </Box>
   );
 }
